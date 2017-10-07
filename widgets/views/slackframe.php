0b0d2c17760a3f2ecce6c4f18ea18e379201c4c8
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-07 11:03:18 +0900

    Update slackframe.php

--- widgets/views/slackframe.php
+++ widgets/views/slackframe.php
@@ -3,11 +3,6 @@
 use yii\helpers\Url;
 use humhub\models\Setting;
 ?>
-<div class="panel">
-  <div class="panel-heading">
-    <?=Yii::t('SlackModule.base', '<strong>Slack</strong> Chat'); ?>
-  </div>
-  <div class="panel-body">
 
 <Code>YOUR CODE HERE</code>
 
