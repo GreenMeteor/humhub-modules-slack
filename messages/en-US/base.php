1c008b5b4e78ef0259420c324410b55528114bd9
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-08 15:54:48 +0900

    Update base.php

--- messages/en-US/base.php
+++ messages/en-US/base.php
@@ -1,5 +1,9 @@
 <?php
+
 return [
-    'sort' => 'Sort',
+    'Slack Settings' => 'Slack Settings',
+    'Slack Widget URL:' => 'Slack Widget URL:',
+    'e.g. https://embed.small.chat/{id}.js' => 'e.g. https://embed.small.chat/{id}.js',
+    '<strong>Slack</strong> module configuration' => '<strong>Slack</strong> module configuration',
     'save' => 'Save',
 ];
