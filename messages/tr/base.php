cf1bfd86855de3d1a3dca31f76773ea536fde560
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-08 15:55:37 +0900

    Update base.php

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -1,5 +1,9 @@
 <?php
+
 return [
-    'sort' => 'Tip',
+    'Slack Settings' => '',
+    'Slack Widget URL:' => '',
+    'e.g. https://embed.small.chat/{id}.js' => 'e.g. https://embed.small.chat/{id}.js',
+    '<strong>Slack</strong> module configuration' => '',
     'save' => 'Kaydet',
 ];
